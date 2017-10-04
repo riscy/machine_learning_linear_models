d3f8ce7da4ec2bc5fac59e5424f60563058c8f7e
riscy <riscy@users.noreply.github.com>
2017-10-04 12:21:23 -0600

    linting; fix python3 compatibility

--- demo.py
+++ demo.py
@@ -16,14 +16,16 @@ import system_identifier
 from scipy import around
 from scipy import size
 from scipy.linalg import norm
+from numpy.random import seed
 
-DIM_X = 20 # dimensionality of input
-DIM_Y = 15 # dimensionality of output
-RANK = 10  # internal rank/bottleneck
+DIM_X = 20                      # dimensionality of input
+DIM_Y = 15                      # dimensionality of output
+RANK = 10                       # internal rank/bottleneck
 NOISE_SCALE = 1.0
 NUM_SAMPLES = 5000
-SPLIT = NUM_SAMPLES/2 # train/test split
-REG = 1e-6 # regularization on the model
+SPLIT = int(NUM_SAMPLES/2)      # train/test split
+REG = 1e-6                      # regularization on the model
+
 
 def sqerr(matrix1, matrix2):
     """Squared error (frobenius norm of diff) between two matrices."""
@@ -31,17 +33,18 @@ def sqerr(matrix1, matrix2):
 
 
 if __name__ == '__main__':
+    seed(10)
     for model in [multivariate_regressor, reduced_rank_regressor, system_identifier]:
         # generate the data:
-        print model.ideal_data.__doc__
+        print(model.ideal_data.__doc__)
         XX, YY = model.ideal_data(NUM_SAMPLES, DIM_X, DIM_Y, RANK, NOISE_SCALE)
         # run each of the regressors against it:
         for regressor in ([
                 multivariate_regressor.MultivariateRegressor(XX[:SPLIT], YY[:SPLIT], REG),
                 reduced_rank_regressor.ReducedRankRegressor(XX[:SPLIT], YY[:SPLIT], RANK, REG),
                 system_identifier.SystemIdentifier(XX[:SPLIT], YY[:SPLIT], RANK, REG)]):
-            print '  {}'.format(regressor)
+            print('  {}'.format(regressor))
             training_error = sqerr(regressor.predict(XX[:SPLIT]), YY[:SPLIT])
             testing_error = sqerr(regressor.predict(XX[SPLIT+1:]), YY[SPLIT+1:])
-            print '    Training error: {}\n    Testing error: {}'\
-                .format(training_error, testing_error)
+            print('    Training error: {}\n    Testing error: {}'
+                  .format(training_error, testing_error))
